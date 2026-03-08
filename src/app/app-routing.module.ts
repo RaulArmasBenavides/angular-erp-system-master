1a341ba9f6c6a731280d72f83166b4efaa262287
RaulArmas <raularmasbx@gmail.com>
2026-03-08 18:51:43 -0500

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ScheduleComponent} from './components/events/schedule/schedule.component'
-import {LeaveComponent} from './components/leave/leave.component'
-import {NotificationComponent} from './components/notifications/notification/notification.component'
+import { ScheduleComponent } from './components/events/schedule/schedule.component';
+// import { LeaveComponent } from './components/leave/leave.component';
+import { NotificationComponent } from './components/notifications/notification/notification.component';
 import { MeetingsComponent } from './components/events/meetings/meetings.component';
 import { NotificationListComponent } from './components/notifications/notification-list/notification-list.component';
 import { AuthComponent } from './auth/auth.component';
@@ -14,22 +14,22 @@ import { CieEntryComponent } from './components/exams/cie-entry/cie-entry.compon
 import { CieMarksListComponent } from './components/cie-marks-list/cie-marks-list.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent },
-  { path: 'schedule', component: ScheduleComponent },
-  { path: 'syllabus', component: SyllabusComponent },
-  { path: 'leave', component: LeaveComponent },
-  { path: 'notification', component: NotificationComponent },
-  { path: 'meetings', component: MeetingsComponent },
-  { path: 'notifications-list', component: NotificationListComponent },
-  { path: 'syllabus-list', component: SyllabusListComponent },
-  { path: 'cie-marks-list', component: CieMarksListComponent },
-  { path: 'ese-list', component: EseListComponent },
-  { path: 'scheduler', component: EseSchedulerComponent },
-  { path: 'cie-entry', component: CieEntryComponent },
+	{ path: '', component: AuthComponent },
+	{ path: 'schedule', component: ScheduleComponent },
+	{ path: 'syllabus', component: SyllabusComponent },
+	// { path: 'leave', component: LeaveComponent },
+	{ path: 'notification', component: NotificationComponent },
+	{ path: 'meetings', component: MeetingsComponent },
+	{ path: 'notifications-list', component: NotificationListComponent },
+	{ path: 'syllabus-list', component: SyllabusListComponent },
+	{ path: 'cie-marks-list', component: CieMarksListComponent },
+	{ path: 'ese-list', component: EseListComponent },
+	{ path: 'scheduler', component: EseSchedulerComponent },
+	{ path: 'cie-entry', component: CieEntryComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
